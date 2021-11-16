4a10d40ee029549d646030bbccdeabf7199981d4
davidtian90 <87958157+davidtian90@users.noreply.github.com>
2021-11-17 02:37:03 +0800

    added delete logic

--- create-ecs-service/index.js
+++ create-ecs-service/index.js
@@ -38,7 +38,7 @@ async function run(){
         customUserAgent: 'amazon-ecs-deploy-task-definition-for-github-actions'
     });
     const cluster = core.getInput("ecs-cluster-name");
-    const serviceName = core.getInput("ecs-service-name");
+    const inServiceName = core.getInput("ecs-service-name");
     const taskDefinition = core.getInput("ecs-task-definition-name");
     
     const {serviceArns} = await ecs.listServices({
@@ -46,6 +46,41 @@ async function run(){
     }).promise();
 
     core.info(`Setting output: serviceArns: ${(serviceArns) || ''}`);
+
+    const arr1 = inServiceName.split("-");
+    const orgNumber = arr1[arr1.length - 1];
+    let serviceNames = [];
+
+    let serviceName;
+
+    if (serviceArns.length) {
+        serviceArns.forEach(serviceArn => {
+            const arr2 = serviceArn.split("-");
+            const number = arr2[arr2.length - 1];
+
+            if (number.startsWith(orgNumber)) {
+                const arr3 = serviceArn.split("/");
+                const serviceName = arr3[arr3.length - 1];
+                serviceNames.push(serviceName);
+            }
+        });
+        serviceNames.sort();
+        // get last service
+        const service = serviceNames[serviceNames.length - 1];
+        // delete serviceName
+        const response = await ecs.deleteService({service, cluster, force:true}).promise();
+        core.info(`Service ${service} is deleted in cluster ${cluster}`);
+        core.info(`Delete service: ${response}`);
+        // generate service name
+        const arr4 = service.split("-");
+        const number = arr4[arr4.length - 1];
+        const newNumber = String(Number(number) + 1);
+        serviceName = service.replace(number, newNumber);
+    }else{
+        serviceName = inServiceName + "000";
+    }
+
+    core.info(`New service name: ${serviceName}`);
 
     try{
       await createService(ecs,{
