382ebd4b342979c3afc97ab2695443fe983f4ad7
davidtian90 <87958157+davidtian90@users.noreply.github.com>
2021-11-18 10:58:52 +0800

    updated again

--- delete-ecs-service/index.js
+++ delete-ecs-service/index.js
@@ -12,6 +12,7 @@ async function descaleService(ecs, {cluster, service}){
 }
 
 async function deleteService(ecs, {cluster, service}){
+  
     const descaleResponse = await descaleService(ecs, {cluster, service});
     const force = true;
     const response = await ecs.deleteService({
@@ -24,12 +25,48 @@ async function deleteService(ecs, {cluster, service}){
 }
 
 async function run(){
-    try{
-        const ecs = new aws.ECS({
-            customUserAgent: 'amazon-ecs-deploy-task-definition-for-github-actions'
+    const ecs = new aws.ECS({
+        customUserAgent: 'amazon-ecs-deploy-task-definition-for-github-actions'
+    });
+    const cluster = core.getInput("ecs-cluster-name");
+    const inServiceName = core.getInput("ecs-service-name");
+    
+    const {serviceArns} = await ecs.listServices({
+        cluster: cluster,
+    }).promise();
+
+    core.info(`Setting output: serviceArns: ${(serviceArns) || ''}`);
+
+    const arr1 = inServiceName.split("-");
+    const orgNumber = arr1[arr1.length - 1];
+    let serviceNames = [];
+
+    let serviceName = undefined;
+
+    if (serviceArns.length) {
+        serviceArns.forEach(serviceArn => {
+            const arr2 = serviceArn.split("-");
+            const number = arr2[arr2.length - 1];
+
+            if (number.startsWith(orgNumber)) {
+                const arr3 = serviceArn.split("/");
+                const serviceName = arr3[arr3.length - 1];
+                serviceNames.push(serviceName);
+            }
         });
-        const cluster = core.getInput("ecs-cluster-name");
-        const serviceName = core.getInput("ecs-service-name");
+
+        if (serviceNames.length) {
+            serviceNames.sort();
+            // get last service
+            const service = serviceNames[serviceNames.length - 1];
+            serviceName = service;
+        }
+    }
+    if (!serviceName) {
+        serviceName = inServiceName + "000";
+    }
+    
+    try{
         await deleteService(ecs, {
             cluster,
             service: serviceName
